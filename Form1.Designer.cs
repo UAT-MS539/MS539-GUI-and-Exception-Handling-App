25226ba5ac2c0aa48dbe7193bc4f4d2e3dc47e3c
Douglas Little <douglas.a.little@gmail.com>
2024-10-28 14:38:58 -0400

    Form 1 Gui complete

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -28,12 +28,177 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
-            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            menuStrip1 = new MenuStrip();
+            fileToolStripMenuItem = new ToolStripMenuItem();
+            clearToolStripMenuItem = new ToolStripMenuItem();
+            exitToolStripMenuItem = new ToolStripMenuItem();
+            editToolStripMenuItem = new ToolStripMenuItem();
+            toolsToolStripMenuItem = new ToolStripMenuItem();
+            helpToolStripMenuItem = new ToolStripMenuItem();
+            aboutToolStripMenuItem = new ToolStripMenuItem();
+            hourLabel = new Label();
+            hourlyRateLabel = new Label();
+            hoursTextBox = new TextBox();
+            hourlyRateTextBox = new TextBox();
+            calculateButton = new Button();
+            clearButton = new Button();
+            exitButton = new Button();
+            menuStrip1.SuspendLayout();
+            SuspendLayout();
+            // 
+            // menuStrip1
+            // 
+            menuStrip1.Items.AddRange(new ToolStripItem[] { fileToolStripMenuItem, editToolStripMenuItem, toolsToolStripMenuItem, helpToolStripMenuItem });
+            menuStrip1.Location = new Point(0, 0);
+            menuStrip1.Name = "menuStrip1";
+            menuStrip1.Size = new Size(346, 24);
+            menuStrip1.TabIndex = 0;
+            menuStrip1.Text = "menuStrip1";
+            // 
+            // fileToolStripMenuItem
+            // 
+            fileToolStripMenuItem.DropDownItems.AddRange(new ToolStripItem[] { clearToolStripMenuItem, exitToolStripMenuItem });
+            fileToolStripMenuItem.Name = "fileToolStripMenuItem";
+            fileToolStripMenuItem.Size = new Size(37, 20);
+            fileToolStripMenuItem.Text = "&File";
+            // 
+            // clearToolStripMenuItem
+            // 
+            clearToolStripMenuItem.Name = "clearToolStripMenuItem";
+            clearToolStripMenuItem.Size = new Size(180, 22);
+            clearToolStripMenuItem.Text = "&Clear";
+            clearToolStripMenuItem.Click += clearToolStripMenuItem_Click;
+            // 
+            // exitToolStripMenuItem
+            // 
+            exitToolStripMenuItem.Name = "exitToolStripMenuItem";
+            exitToolStripMenuItem.Size = new Size(180, 22);
+            exitToolStripMenuItem.Text = "E&xit";
+            exitToolStripMenuItem.Click += exitToolStripMenuItem_Click;
+            // 
+            // editToolStripMenuItem
+            // 
+            editToolStripMenuItem.Name = "editToolStripMenuItem";
+            editToolStripMenuItem.Size = new Size(39, 20);
+            editToolStripMenuItem.Text = "&Edit";
+            // 
+            // toolsToolStripMenuItem
+            // 
+            toolsToolStripMenuItem.Name = "toolsToolStripMenuItem";
+            toolsToolStripMenuItem.Size = new Size(46, 20);
+            toolsToolStripMenuItem.Text = "&Tools";
+            // 
+            // helpToolStripMenuItem
+            // 
+            helpToolStripMenuItem.DropDownItems.AddRange(new ToolStripItem[] { aboutToolStripMenuItem });
+            helpToolStripMenuItem.Name = "helpToolStripMenuItem";
+            helpToolStripMenuItem.Size = new Size(44, 20);
+            helpToolStripMenuItem.Text = "&Help";
+            // 
+            // aboutToolStripMenuItem
+            // 
+            aboutToolStripMenuItem.Name = "aboutToolStripMenuItem";
+            aboutToolStripMenuItem.Size = new Size(107, 22);
+            aboutToolStripMenuItem.Text = "&About";
+            aboutToolStripMenuItem.Click += aboutToolStripMenuItem_Click;
+            // 
+            // hourLabel
+            // 
+            hourLabel.AutoSize = true;
+            hourLabel.Location = new Point(28, 79);
+            hourLabel.Name = "hourLabel";
+            hourLabel.Size = new Size(112, 15);
+            hourLabel.TabIndex = 1;
+            hourLabel.Text = "Enter hours worked:";
+            // 
+            // hourlyRateLabel
+            // 
+            hourlyRateLabel.AutoSize = true;
+            hourlyRateLabel.Location = new Point(43, 123);
+            hourlyRateLabel.Name = "hourlyRateLabel";
+            hourlyRateLabel.Size = new Size(97, 15);
+            hourlyRateLabel.TabIndex = 2;
+            hourlyRateLabel.Text = "Enter hourly rate:";
+            // 
+            // hoursTextBox
+            // 
+            hoursTextBox.Location = new Point(165, 76);
+            hoursTextBox.Name = "hoursTextBox";
+            hoursTextBox.Size = new Size(100, 23);
+            hoursTextBox.TabIndex = 3;
+            // 
+            // hourlyRateTextBox
+            // 
+            hourlyRateTextBox.Location = new Point(165, 120);
+            hourlyRateTextBox.Name = "hourlyRateTextBox";
+            hourlyRateTextBox.Size = new Size(100, 23);
+            hourlyRateTextBox.TabIndex = 4;
+            // 
+            // calculateButton
+            // 
+            calculateButton.Location = new Point(43, 191);
+            calculateButton.Name = "calculateButton";
+            calculateButton.Size = new Size(75, 23);
+            calculateButton.TabIndex = 5;
+            calculateButton.Text = "Calculate";
+            calculateButton.UseVisualStyleBackColor = true;
+            // 
+            // clearButton
+            // 
+            clearButton.Location = new Point(124, 191);
+            clearButton.Name = "clearButton";
+            clearButton.Size = new Size(75, 23);
+            clearButton.TabIndex = 6;
+            clearButton.Text = "Clear";
+            clearButton.UseVisualStyleBackColor = true;
+            // 
+            // exitButton
+            // 
+            exitButton.Location = new Point(214, 191);
+            exitButton.Name = "exitButton";
+            exitButton.Size = new Size(75, 23);
+            exitButton.TabIndex = 7;
+            exitButton.Text = "Exit";
+            exitButton.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(346, 301);
+            Controls.Add(exitButton);
+            Controls.Add(clearButton);
+            Controls.Add(calculateButton);
+            Controls.Add(hourlyRateTextBox);
+            Controls.Add(hoursTextBox);
+            Controls.Add(hourlyRateLabel);
+            Controls.Add(hourLabel);
+            Controls.Add(menuStrip1);
+            MainMenuStrip = menuStrip1;
+            Name = "Form1";
+            Text = "Form1";
+            menuStrip1.ResumeLayout(false);
+            menuStrip1.PerformLayout();
+            ResumeLayout(false);
+            PerformLayout();
         }
 
         #endregion
+
+        private MenuStrip menuStrip1;
+        private ToolStripMenuItem fileToolStripMenuItem;
+        private ToolStripMenuItem clearToolStripMenuItem;
+        private ToolStripMenuItem exitToolStripMenuItem;
+        private ToolStripMenuItem editToolStripMenuItem;
+        private ToolStripMenuItem toolsToolStripMenuItem;
+        private ToolStripMenuItem helpToolStripMenuItem;
+        private ToolStripMenuItem aboutToolStripMenuItem;
+        private Label hourLabel;
+        private Label hourlyRateLabel;
+        private TextBox hoursTextBox;
+        private TextBox hourlyRateTextBox;
+        private Button calculateButton;
+        private Button clearButton;
+        private Button exitButton;
     }
 }
--- Form1.cs
+++ Form1.cs
@@ -1,10 +1,30 @@
 namespace MS539_GUI_and_Exception_Handling_App
 {
+    /*Douglas Little
+ * MS539 Gui and Exception Hndling App
+ * I estimate this will take me about an hour to finish
+ * 
+ */
     public partial class Form1 : Form
     {
         public Form1()
         {
             InitializeComponent();
+        }
+
+        private void clearToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void exitToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            this.close
+        }
+
+        private void aboutToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+
         }
     }
 }
