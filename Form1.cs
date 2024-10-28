5e21a1326f8ef6b42133884e91e018a19b4a9b82
Douglas Little <douglas.a.little@gmail.com>
2024-10-28 14:21:42 -0400

    Add project files.

---
+++ Form1.cs
@@ -0,0 +1,10 @@
+namespace MS539_GUI_and_Exception_Handling_App
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+    }
+}
